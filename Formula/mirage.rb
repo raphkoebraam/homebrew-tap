2fc3f1be46303ff239aeaa3a3e5d664da07df2ab
Raphael Koebraam <5387088+raphkoebraam@users.noreply.github.com>
2026-08-16 12:08:48 +0200

    chore: mirage 0.9.0

--- Formula/mirage.rb
+++ Formula/mirage.rb
@@ -2,7 +2,7 @@ class Mirage < Formula
   desc "Manage Apple simulators by name, not UDID"
   homepage "https://github.com/raphkoebraam/Mirage"
   url "https://github.com/raphkoebraam/Mirage/releases/download/v0.9.0/mirage-0.9.0-macos.tar.gz"
-  sha256 "b4413b891090a68ffb7d00222eb1c4535ad80a1e0245bd0b7d30e27e032ddc8f"
+  sha256 "e3130cb767c251e143df9dc8c370716c7931a58a622565745c83c78989a31615"
   license "MIT"
 
   # The binary is built against the macOS 15 SDK and uses Synchronization.Mutex.
